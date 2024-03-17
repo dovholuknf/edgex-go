e1bab49a7fa4f5b7076c9c3773f225b805cb345c
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-03-17 12:33:34 -0400

    fix: retsore changes made during testing found in PR comments fix: move SecurityOptions to all-services fix: restore default ports fix: restore imports
    Signed-off-by: dovholuknf <46322585+dovholuknf@users.noreply.github.com>

--- internal/support/scheduler/application/scheduler/manager.go
+++ internal/support/scheduler/application/scheduler/manager.go
@@ -6,7 +6,6 @@
 package scheduler
 
 import (
-	clientInterfaces "github.com/edgexfoundry/go-mod-core-contracts/v3/clients/interfaces"
 	"sync"
 	"time"
 
@@ -16,6 +15,8 @@ import (
 
 	bootstrapInterfaces "github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/interfaces"
 	"github.com/edgexfoundry/go-mod-bootstrap/v3/bootstrap/secret"
+
+	clientInterfaces "github.com/edgexfoundry/go-mod-core-contracts/v3/clients/interfaces"
 
 	"github.com/edgexfoundry/go-mod-core-contracts/v3/clients/logger"
 	"github.com/edgexfoundry/go-mod-core-contracts/v3/common"
